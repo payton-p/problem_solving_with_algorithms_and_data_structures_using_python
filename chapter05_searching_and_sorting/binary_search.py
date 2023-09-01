c77e1ea443e508e25688ca18d00bfde7040c8efa
Payton Pietron <paytonpietron@gmail.com>
2023-09-01 17:11:15 -0400

    Binary search

---
+++ chapter05_searching_and_sorting/binary_search.py
@@ -0,0 +1,27 @@
+def binary_search(ordered_list, item):
+    first = 0
+    last = len(ordered_list) - 1
+    found = False
+
+    while first <= last and not found:
+        midpoint = (first + last) // 2
+        if ordered_list[midpoint] == item:
+            found = True
+        else:
+            if item < ordered_list[midpoint]:
+                last = midpoint - 1
+            else:
+                first = midpoint + 1
+
+    return found
+
+
+def main():
+    # Binary search of an ordered list. Time is O(log(n)).
+    ordered_list = [0, 1, 2, 8, 13, 17, 19, 32, 42, ]
+    print(binary_search(ordered_list, 3))
+    print(binary_search(ordered_list, 13))
+
+
+if __name__ == "__main__":
+    main()
